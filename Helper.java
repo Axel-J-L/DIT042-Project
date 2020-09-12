21e137b661320f2c93e56d129b6475f55058e545
Altansukh T <altansukh@altansukh.com>
2020-09-13 01:09:25 +0800

    Helper functions

--- Helper.java
+++ Helper.java
@@ -19,14 +19,28 @@ public class Helper {
      * @param char 
      * @param string
      */
-    public void getInput() {
-        // TODO implement here
+    public static String getInput(String message) {
+    	
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println(message);
+
+        String userInput = scanner.nextLine();  // Read user input
+        
+        scanner.close(); // Close scanner
+        
+        return userInput;  // Output user input
+        
     }
 
     /**
      * 
      */
-    public void printArray() {
+    public void printArray(String[] pArray[]) {
+    	
+    	for (String[] element: pArray) {
+            System.out.println(element);
+        }
+    	
         // TODO implement here
     }
 

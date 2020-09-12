8936fe7ad4578406c4c212d78b067a18df7d4494
Axel-J-L <70752905+Axel-J-L@users.noreply.github.com>
2020-09-12 23:35:13 +0200

    Started

--- {code}pendent/src/Axel/EmployeeMenu.java
+++ {code}pendent/src/Axel/EmployeeMenu.java
@@ -1,4 +1,30 @@
 package Axel;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class EmployeeMenu {
-}
+
+    Scanner input= new Scanner(System.in);
+
+    public static String[] empMenu(){
+
+        String[] menu= new String[7];
+
+        menu[0]=("1. Register a game" );
+        menu[1]=("2. Remove a game");
+        menu[2]=("3. Register a customer");
+        menu[3]=("4. Remove a customer ");
+        menu[4]=("5. Show total rent profit");
+        menu[5]=("6. View all games");
+        menu[6]=("7. Return to Main Menu");
+
+        System.out.println(menu);
+
+
+       return menu;
+     }
+
+
+    }
+
+

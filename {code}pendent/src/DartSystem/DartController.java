0c4eec4a9e76f9b74c748b9b0785f4b9b4458e83
Drake <drakeaxelrod@gmail.com>
2020-09-13 08:50:20 +0200

    Working on menu Object

--- {code}pendent/src/DartSystem/DartController.java
+++ {code}pendent/src/DartSystem/DartController.java
@@ -15,69 +15,106 @@ public class DartController {
 
     public static void DartController() {
 
-        Scanner input = new Scanner(System.in);
-        int menuC = 0;
-        // going to modify this so that if the wrong password is entered it takes you back to the main menu (drake)
-        while (menuC != 1 || menuC != 2 || menuC != 3 || menuC != 4) {
-            System.out.println("<<< Main Menu >>>");
-            System.out.println("Welcome to DART, your good old game rental system. The competition has no steam to keep up!");
-            System.out.println(" ");
-            System.out.println("Please specify your role by entering one of the options given:");
-            System.out.println("1. Enter \"M\" for DartSystem.Manager ");
-            System.out.println("2. Enter \"E\" for DartSystem.Employee");
-            System.out.println("3. Enter \"C\" for DartSystem.Customer");
-            System.out.println("4. Enter \"X\" to exit system");
-            System.out.print("Enter: ");
-            menuC = input.nextInt();
-            input.nextLine();
-            if (menuC == 1) {
-                System.out.print("password: ");
-                String password = input.nextLine();
-            } else if (menuC == 2) {
-                // generate employee menu
-            } else if (menuC == 3) {
-                // generate customer menu
-            } else if (menuC == 4) {
-                // exit system
-            } else {
-                System.out.println("");
-            }
-        }
+
     }
 
-    /**
-     * 
-     */
-    public String menuState;
+//    going to modify this so that if the wrong password is entered it takes you back to the main menu (drake)
+//        Scanner input = new Scanner(System.in);
+//        int menuC = 0;
+//        while (menuC != 1 || menuC != 2 || menuC != 3 || menuC != 4) {
+//            System.out.println("<<< Main Menu >>>");
+//            System.out.println("Welcome to DART, your good old game rental system. The competition has no steam to keep up!");
+//            System.out.println(" ");
+//            System.out.println("Please specify your role by entering one of the options given:");
+//            System.out.println("1. Enter \"M\" for DartSystem.Manager ");
+//            System.out.println("2. Enter \"E\" for DartSystem.Employee");
+//            System.out.println("3. Enter \"C\" for DartSystem.Customer");
+//            System.out.println("4. Enter \"X\" to exit system");
+//            System.out.print("Enter: ");
+//            menuC = input.nextInt();
+//            input.nextLine();
+//            if (menuC == 1) {
+//                System.out.print("password: ");
+//                String password = input.nextLine();
+//            } else if (menuC == 2) {
+//                // generate employee menu
+//            } else if (menuC == 3) {
+//                // generate customer menu
+//            } else if (menuC == 4) {
+//                // exit system
+//            } else {
+//                System.out.println("");
+//            }
+//        }
 
-    /**
-     * 
-     */
-    public String menuOption;
 
-    String cEmployee;                       //create employee
-    String rEmployee;                       //remove employee
+    public static void mainMenu() {
+        String title = "<<< Main Menu >>>";
+        String slogan = "Welcome to DART, your good old game rental system. The competition has no steam to keep up!";
+        String divider = " ";
+        String askUser = "Please specify your role by entering one of the options given:";
+        String optionOne = "1. Enter \"M\" for DartSystem.Manager ";
+        String optionTwo = "2. Enter \"E\" for DartSystem.Employee";
+        String optionThree = "3. Enter \"C\" for DartSystem.Customer";
+        String optionFour = "4. Enter \"X\" to exit system";
+        String userInput = "Enter: ";
 
-
-    /**
-     * 
-     */
-    public void mainMenu() {
-        // TODO implement here
     }
 
-    /**
-     * 
-     */
-    public void exit() {
-        // TODO implement here
-    }
 
-    /**
-     * 
-     */
-    public void exceptionHandler() {
-        // TODO implement here
-    }
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//    /**
+//     *
+//     */
+//    public String menuState;
+//
+//    /**
+//     *
+//     */
+//    public String menuOption;
+//
+//
+//
+//    /**
+//     *
+//     */
+//    public void mainMenu() {
+//        // TODO implement here
+//    }
+//
+//    /**
+//     *
+//     */
+//    public void exit() {
+//        // TODO implement here
+//    }
+//
+//    /**
+//     *
+//     */
+//    public void exceptionHandler() {
+//        // TODO implement here
+//    }
+//
+//}

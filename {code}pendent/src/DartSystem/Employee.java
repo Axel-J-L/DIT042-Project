e1b21279ffc431b4627b754d0528d5ab42e77d47
Axel-J-L <70752905+Axel-J-L@users.noreply.github.com>
2020-09-13 22:10:31 +0200

    Added a switch to the employee menu to accept different choices.

--- {code}pendent/src/DartSystem/Employee.java
+++ {code}pendent/src/DartSystem/Employee.java
@@ -61,13 +61,43 @@ public class Employee {
         // TODO implement here
         System.out.println("<<< Employee menu >>>");
         System.out.println("Employee screen - Type one of the options below:");
-        System.out.println("1. Enter \"1\" to register a game ");
-        System.out.println("2. Enter \"2\" to remove a game");
-        System.out.println("3. Enter \"3\" to register a customer");
-        System.out.println("4. Enter \"4\" to remove a customer");
-        System.out.println("5. Enter \"5\" to show total rent profit");
-        System.out.println("6. Enter \"6\" to view all games");
-        System.out.println("7. Enter \"7\" to return to main menu\n");
+        System.out.println("1. Enter \"A\" to register a game ");
+        System.out.println("2. Enter \"B\" to remove a game");
+        System.out.println("3. Enter \"C\" to register a customer");
+        System.out.println("4. Enter \"D\" to remove a customer");
+        System.out.println("5. Enter \"E\" to show total rent profit");
+        System.out.println("6. Enter \"F\" to view all games");
+        System.out.println("7. Enter \"R\" to return to main menu\n");
+        String[] employeeMenuAcceptSet = {"A","a","B","b","C","c","D","d","E","e","F","f","R","r"}; // Accepted responses for menu options
+        Helper getInput = new Helper(); // Getting new object from Helper
+        String userInput = getInput.getMenuInput("Please enter your choice: ", employeeMenuAcceptSet); // using argument to call Helper method
+
+        switch(userInput.toLowerCase())
+        {
+            case "a":
+                System.out.println("Register a game");
+                break;
+            case "b":
+                System.out.println("Remove game");
+                break;
+            case "c":
+                System.out.println("Register a customer");
+                break;
+            case "d":
+                System.out.println("Remove customer: ");
+                break;
+            case "e":
+                System.out.println("Show total rent profit");
+                break;
+            case "f":
+                System.out.println("View all games: ");
+                break;
+            case "r":
+                System.out.println("Return to main menu: ");
+                break;
+            default:
+                System.out.println("no match");
+        }
     }
 
     /**
@@ -75,6 +105,8 @@ public class Employee {
      */
     public void addEmployee() {
         // TODO implement here
+        System.out.println("Employee name: ");
+        System.out.println();
     }
 
     /**
@@ -82,9 +114,10 @@ public class Employee {
      */
     public void authEmployee() { //Checks and uses the Authenticator method from help class. Returns false if !=password (A)
         // TODO implement here
-        String password ="password123";
+
         Helper passScan = new Helper();
-        String userResponse = passScan.Authenticator();
+        String userResponse = passScan.Password();
+
 
     }
 

51a5076b9585b42c6a5e80e4c51a6d1389a53055
Altansukh T <altansukh@altansukh.com>
2020-09-13 17:24:43 +0800

    MainMenu
    - Helper - menu input method

--- {code}pendent/src/DartSystem/DartController.java
+++ {code}pendent/src/DartSystem/DartController.java
@@ -16,15 +16,34 @@ public class DartController {
     public static void DartController() {
 
             System.out.println("<<< Main Menu >>>");
-            System.out.println("Welcome to DART, your good old game rental system. The competition has no steam to keep up!");
-            System.out.println(" ");
+            System.out.println("Welcome to DART, your good old game rental system. The competition has no steam to keep up! \n");
             System.out.println("Please specify your role by entering one of the options given:");
             System.out.println("1. Enter \"M\" for DartSystem.Manager ");
             System.out.println("2. Enter \"E\" for DartSystem.Employee");
             System.out.println("3. Enter \"C\" for DartSystem.Customer");
-            System.out.println("4. Enter \"X\" to exit system");
-            System.out.print("Enter: ");
+            System.out.println("4. Enter \"X\" to exit system \n");
 
+            String[] mainMenuAcceptSet = {"M","m","E","e","C","c","X","x"}; // Accepted responses for menu options
+            Helper getinput = new Helper(); // Creating new Helper object
+            String userInput = getinput.getMenuInput("Please enter your choice: ", mainMenuAcceptSet); // Calling Helper method
+
+            switch(userInput.toLowerCase())
+            {
+                case "m":
+                    System.out.println("Let's Go to Manager section");
+                    break;
+                case "e":
+                    System.out.println("Let's Go to Emnployee section");
+                    break;
+                case "c":
+                    System.out.println("Let's Go to Customer section");
+                    break;
+                case "x":
+                    System.out.println("Exiting system ...");
+                    break;
+                default:
+                    System.out.println("no match");
+            }
     }
 }
 

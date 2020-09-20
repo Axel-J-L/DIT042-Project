a2c00b45a0b74bbd7c04008f56d1593e978ecdf5
Drake <drakeaxelrod@gmail.com>
2020-09-20 17:12:00 +0200

    added employee array to launcher and initial array size

--- {code}pendent/src/DartSystem/Launcher.java
+++ {code}pendent/src/DartSystem/Launcher.java
@@ -6,9 +6,11 @@ import java.lang.*;
 import java.net.Authenticator;
 import java.util.Scanner;
 
-
 public class Launcher {
-	
+    final public static int INITIAL_ARRAY_SIZE = 4;
+    Employee[] employees =  new Employee[INITIAL_ARRAY_SIZE];
+
+
     public static void main(String[] args) {
         // TODO The main you can run the program to test things from here if you want
         DartController session = new DartController();
